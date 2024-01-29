f4f005b9af6c9be6b02f352f9a131fc82f10704c
Luiz Cesar <luiizcesar.lc@gmail.com>
2024-01-29 19:10:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let modal = document.querySelector('.modal');
+
+console.log(modal)
+
+let iconText = document.getElementsByClassName('icon-text');
+console.log(iconText)
+
+let checkIcon = document.querySelector('.check-icon');
+console.log(checkIcon)
